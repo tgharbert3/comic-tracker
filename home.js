e8b23972a53f79868a83605d98819f4b6545087d
Tyler Harbert <tyler.g.harbert@gmail.com>
2024-08-27 18:34:11 -0400

    inital commit to add files

---
+++ home.js
@@ -0,0 +1,11 @@
+const marvel_logo = document.querySelector("#marvel-logo")
+const sign_up = document.querySelector("#sign-up")
+
+marvel_logo.onclick = () => {
+    location.href = "./home.html"
+}
+
+sign_up.onclick = function () {
+    location.href = "./sign-up/sign-up.html"
+}
+
---
+++ sign-up/sign-up.js
@@ -0,0 +1,6 @@
+const marvel_logo = document.querySelector("#marvel-logo")
+
+
+marvel_logo.onclick = function () {
+    location.href = "../home.html"
+}
