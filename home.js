86370608f7c87a549777b10b333b66707802eab8
Tyler Harbert <tyler.g.harbert@gmail.com>
2024-08-28 13:37:40 -0400

    completed nav bar and added files for all pages, may need to change pointer

--- home.js
+++ home.js
@@ -1,11 +1,2 @@
-const marvel_logo = document.querySelector("#marvel-logo")
-const sign_up = document.querySelector("#sign-up")
 
-marvel_logo.onclick = () => {
-    location.href = "./home.html"
-}
-
-sign_up.onclick = function () {
-    location.href = "./sign-up/sign-up.html"
-}
 
--- sign-up/sign-up.js
+++
@@ -1,6 +0,0 @@
-const marvel_logo = document.querySelector("#marvel-logo")
-
-
-marvel_logo.onclick = function () {
-    location.href = "../home.html"
-}
